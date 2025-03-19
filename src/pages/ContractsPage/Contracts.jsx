823b8bdfc4dc76f7efccdc8038bcddada224f910
Alanis Yates <yatesalanis@gmail.com>
2025-03-18 19:50:28 -0700

    Add in the new image component

--- src/pages/ContractsPage/Contracts.jsx
+++ src/pages/ContractsPage/Contracts.jsx
@@ -3,7 +3,7 @@ import { ContractListWidget } from "./ContractListWidget";
 
 export const ContractPage = () => {
   return (
-    <Box sx={{ width: "100%", height: '100vh' }}>
+    <Box sx={{ width: "100%", height: "100vh" }}>
       <Typography variant="h1">Contracts</Typography>
       <ContractListWidget />
     </Box>
