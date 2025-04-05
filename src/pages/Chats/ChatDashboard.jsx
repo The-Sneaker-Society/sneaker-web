49fa5b68640af717fa48b4f3c0d7fb82ec9782f2
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2025-04-05 13:29:21 -0700

    Update chat styling (#107)
    * wip
    
    * add new chat component

--- src/pages/Chats/ChatDashboard.jsx
+++ src/pages/Chats/ChatDashboard.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
 import ChatSidebar from "./ChatSidebar";
 import Chat from "../MemberChat/Chat";
+import ChatInterface from "../MemberChat/ChatInterface";
 
 export const ChatDashboard = () => {
   return (
     <>
-      <Chat />
+      <ChatInterface />
     </>
   );
 };
