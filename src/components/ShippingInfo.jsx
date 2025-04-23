017e03f9db7f44168b0f1d7267b71c643bbd60cf
Richard Colachagua <richardcolachagua@gmail.com>
2025-04-23 15:32:22 -0400

    updated logic to reflect update profile page logic to refetch carrier and tracking number

--- src/components/ShippingInfo.jsx
+++ src/components/ShippingInfo.jsx
@@ -1,8 +1,90 @@
-import React from "react";
-import { TextField, Typography, Container, Grid, Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  TextField,
+  Typography,
+  Container,
+  Grid,
+  Box,
+  CircularProgress,
+  Alert,
+} from "@mui/material";
+import * as Yup from "yup";
 import StyledButton from "../pages/HomePage/StyledButton";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_CONTRACT_DETAILS } from "./graphql/getContractDetails";
+import { UPDATE_SHIPPING_INFO } from "./graphql/updateShippingInfo";
+
+const FormikTextField = ({ name, ...props }) => {
+  const [field, meta] = useField(name);
+  const isError = meta.touched && meta.error;
+
+  return (
+    <TextField
+      {...field}
+      {...props}
+      error={isError}
+      helperText={isError ? meta.error : props.helperText}
+      fullWidth
+      sx={{
+        mb: 2,
+        input: { color: "white" },
+        "& .MuiOutlinedInput-root": {
+          "& fieldset": { borderColor: "#fff" },
+          "&:hover fieldset": { borderColor: "#FFD700" },
+        },
+      }}
+      InputLabelProps={{ style: { color: "#aaa" } }}
+    />
+  );
+};
 
 const ShippingInfo = () => {
+  const { data, loading, error, refetch } = useQuery(GET_CONTRACT_DETAILS);
+  const [updateShipping] = useMutation(UPDATE_SHIPPING_INFO);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const validationSchema = Yup.object({
+    carrier: Yup.string().required("Carrier is required"),
+    trackingNumber: Yup.string().required("Tracking number is required"),
+  });
+
+  if (loading) return <CircularProgress />;
+  if (error)
+    return <Alert severity="error">Error loading contract data.</Alert>;
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      const { data } = await updateShipping({
+        variables: {
+          data: {
+            carrier: values.carrier,
+            trackingNumber: values.trackingNumber,
+          },
+        },
+        update: (cache, { data: { updateShippingInfo } }) => {
+          cache.writeQuery({
+            query: GET_CONTRACT_DETAILS,
+            data: { contractDetails: updateShippingInfo },
+          });
+        },
+      });
+
+      if (data?.updateShippingInfo) {
+        setSuccessMessage("Shipping updated successfully");
+        setErrorMessage(null);
+        refetch(); // Refetch the current member data
+      } else {
+        throw new Error("Failed to shipping ingo");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setSuccessMessage(null);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <Container
       maxWidth="sm"
@@ -11,7 +93,7 @@ const ShippingInfo = () => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#000",
+        height: "100vh",
       }}
     >
       <Box
@@ -31,43 +113,41 @@ const ShippingInfo = () => {
         >
           Shipping Info
         </Typography>
-        <TextField
-          label="Shipping Carrier"
-          variant="outlined"
-          fullWidth
-          InputLabelProps={{ style: { color: "#aaa" } }}
-          sx={{
-            mb: 2,
-            input: { color: "white" },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "#fff",
-              },
-              "&:hover fieldset": {
-                borderColor: "#FFD700",
-              },
-            },
+        {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+        {successMessage && <Alert severity="success">{successMessage}</Alert>}
+
+        <Formik
+          initialValues={{
+            carrier: "",
+            trackingNumber: "",
           }}
-        />
-        <TextField
-          label="Tracking Number"
-          variant="outlined"
-          fullWidth
-          InputLabelProps={{ style: { color: "#aaa" } }}
-          sx={{
-            mb: 4,
-            input: { color: "white" },
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "#fff",
-              },
-              "&:hover fieldset": {
-                borderColor: "#FFD700",
-              },
-            },
-          }}
-        />
-        <StyledButton>Confirm</StyledButton>
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <FormikTextField
+                name="carrier"
+                label="Shipping Carrier"
+                variant="outlined"
+                fullWidth
+              />
+              <FormikTextField
+                name="trackingNumber"
+                label="Tracking Number"
+                variant="outlined"
+                fullWidth
+              />
+              <StyledButton
+                type="submit"
+                variant="contained"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? <CircularProgress size={24} /> : "Confirm"}
+              </StyledButton>
+            </Form>
+          )}
+        </Formik>
       </Box>
     </Container>
   );
